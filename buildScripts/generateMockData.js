c38977ad379616b66ba04aef3983b7e8c08c50f9
Tomasz Tomczykiewicz <tomaszek92@gmail.com>
2017-08-24 19:45:01 +0200

    Chapter 11

---
+++ buildScripts/generateMockData.js
@@ -0,0 +1,14 @@
+import jsf from "json-schema-faker";
+import {schema} from "./mockDataSchema";
+import fs from "fs";
+import chalk from "chalk";
+
+const json = JSON.stringify(jsf(schema));
+
+fs.writeFile("./src/api/db.json", json, function(error) {
+   if (error) {
+      return console.log(chalk.red(error));
+   } else {
+      console.log(chalk.green("Mock data generated."));
+   }
+});
